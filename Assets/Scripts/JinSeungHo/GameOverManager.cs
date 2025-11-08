fb718f6d849ddf70e41ffd758c1b5e9debb3061c
BBBB0601 <wlstmdgh1@gmail.com>
2025-11-08 20:14:11 +0900

    JinSeungHo_BubbleDelete_v4
    기존의 버블의 갯수를 카운팅하는 코드를 변경했습니다.

--- Assets/Scripts/JinSeungHo/GameOverManager.cs
+++ Assets/Scripts/JinSeungHo/GameOverManager.cs
@@ -5,6 +5,7 @@ public class GameOverManager : MonoBehaviour
     public GameObject leftBoxArea;
     public GameObject rightBoxArea;
     public float maxDiff = 10;
+    public bool isGameOver = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,6 +25,7 @@ public class GameOverManager : MonoBehaviour
         // 여기에 게임 오버시 실행할 것들 작성
         if(diff >= maxDiff)
         {
+            isGameOver = true;
             Debug.Log("게임 오버");
         }
     }
